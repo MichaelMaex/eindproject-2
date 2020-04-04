fccf4eec97715805b3b2a419b84b9e72adf2c00f
RuneBuckinx <61659178+RuneBuckinx@users.noreply.github.com>
2020-04-04 17:20:18 +0200

    Low press blastwave code and notebook

---
+++ pluto-definitions/blastwave-lowpress/definitions.h
@@ -0,0 +1,31 @@
+#define  PHYSICS                        HD
+#define  DIMENSIONS                     2
+#define  COMPONENTS                     2
+#define  GEOMETRY                       CARTESIAN
+#define  BODY_FORCE                     NO
+#define  FORCED_TURB                    NO
+#define  COOLING                        NO
+#define  RECONSTRUCTION                 LINEAR
+#define  TIME_STEPPING                  RK2
+#define  DIMENSIONAL_SPLITTING          NO
+#define  NTRACER                        0
+#define  USER_DEF_PARAMETERS            3
+
+/* -- physics dependent declarations -- */
+
+#define  EOS                            IDEAL
+#define  ENTROPY_SWITCH                 NO
+#define  THERMAL_CONDUCTION             NO
+#define  VISCOSITY                      NO
+#define  ROTATING_FRAME                 NO
+
+/* -- user-defined parameters (labels) -- */
+
+#define  P_IN                           0
+#define  P_OUT                          1
+#define  GAMMA                          2
+
+/* [Beg] user-defined constants (do not change this line) */
+
+
+/* [End] user-defined constants (do not change this line) */
