32a0191168ff0262c7ee8dd27b6567fc5cf209a5
Michaël Maex <michaelmaex@Michaels-iMac.home>
2020-04-12 09:43:21 +0200

    add highres blastwave definitions

--- pluto-definitions/blastwave_highres/definitions.h
+++ pluto-definitions/blastwave_highres/definitions.h
@@ -1,6 +1,6 @@
 #define  PHYSICS                        MHD
 #define  DIMENSIONS                     2
-#define  COMPONENTS                     2
+#define  COMPONENTS                     3
 #define  GEOMETRY                       CARTESIAN
 #define  BODY_FORCE                     NO
 #define  FORCED_TURB                    NO
@@ -9,14 +9,14 @@
 #define  TIME_STEPPING                  RK2
 #define  DIMENSIONAL_SPLITTING          NO
 #define  NTRACER                        0
-#define  USER_DEF_PARAMETERS            3
+#define  USER_DEF_PARAMETERS            4
 
 /* -- physics dependent declarations -- */
 
 #define  EOS                            IDEAL
 #define  ENTROPY_SWITCH                 NO
-#define  DIVB_CONTROL                   CONSTRAINED_TRANSPORT
-#define  BACKGROUND_FIELD               YES
+#define  DIVB_CONTROL                   DIV_CLEANING
+#define  BACKGROUND_FIELD               NO
 #define  AMBIPOLAR_DIFFUSION            NO
 #define  RESISTIVITY                    NO
 #define  HALL_MHD                       NO
@@ -29,11 +29,9 @@
 #define  P_IN                           0
 #define  P_OUT                          1
 #define  GAMMA                          2
+#define  MU                             3
 
 /* [Beg] user-defined constants (do not change this line) */
 
-#define  LIMITER                        MC_LIM
-#define  CT_EMF_AVERAGE                 ARITHMETIC
-#define  ASSIGN_VECTOR_POTENTIAL        NO
 
 /* [End] user-defined constants (do not change this line) */
