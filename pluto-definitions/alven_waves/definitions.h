6faf394f0d1f2e26fc5a441c54ad32ae2f1501b0
Michaël Maex <michaelmaex@Michaels-iMac.home>
2020-04-15 10:35:59 +0200

    made new alven wave definition

---
+++ pluto-definitions/alven_waves/definitions.h
@@ -0,0 +1,37 @@
+#define  PHYSICS                        MHD
+#define  DIMENSIONS                     2
+#define  COMPONENTS                     3
+#define  GEOMETRY                       CARTESIAN
+#define  BODY_FORCE                     NO
+#define  FORCED_TURB                    NO
+#define  COOLING                        NO
+#define  RECONSTRUCTION                 LINEAR
+#define  TIME_STEPPING                  RK2
+#define  DIMENSIONAL_SPLITTING          NO
+#define  NTRACER                        0
+#define  USER_DEF_PARAMETERS            4
+
+/* -- physics dependent declarations -- */
+
+#define  EOS                            IDEAL
+#define  ENTROPY_SWITCH                 NO
+#define  DIVB_CONTROL                   DIV_CLEANING
+#define  BACKGROUND_FIELD               NO
+#define  AMBIPOLAR_DIFFUSION            NO
+#define  RESISTIVITY                    NO
+#define  HALL_MHD                       NO
+#define  THERMAL_CONDUCTION             NO
+#define  VISCOSITY                      NO
+#define  ROTATING_FRAME                 NO
+
+/* -- user-defined parameters (labels) -- */
+
+#define  B2_IN                          0
+#define  P_OUT                          1
+#define  GAMMA                          2
+#define  BETA                           3
+
+/* [Beg] user-defined constants (do not change this line) */
+
+
+/* [End] user-defined constants (do not change this line) */
